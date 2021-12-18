c1afda3c0c2794177503c30044fe5e2cbcf3dd82
Tahir Mahmood <30719791+tahirmahm123@users.noreply.github.com>
2021-12-18 23:17:59 +0500

    Models Created

--- config/database.js
+++ config/database.js
@@ -4,12 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "mad"),
+        username: env("DATABASE_USERNAME", "mad"),
+        password: env("DATABASE_PASSWORD", ""),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
